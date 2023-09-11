c65716dc702caa63cad746c48fb15225cdec1548
Eric Chang <fsc8504@gmail.com>
2023-09-11 14:41:29 -0700

    Add basic cards animation

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import cards from '../../../assets/cards/index.js';
 
 function Table({ communityCards, pot }) {
   return (

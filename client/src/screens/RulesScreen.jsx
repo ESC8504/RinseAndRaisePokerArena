a1bc14d100829db85b222ae963765c6505e1d294
Eric Chang <fsc8504@gmail.com>
2023-09-16 14:47:39 -0700

    Add rule screen

---
+++ client/src/screens/RulesScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, Image } from 'react-native';
+
+function RulesScreen() {
+  return (
+    <View>
+      <Image
+        source={require('../../../assets/ranking.png')}
+        style={styles.image}
+        resizeMode="contain"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default RulesScreen;
